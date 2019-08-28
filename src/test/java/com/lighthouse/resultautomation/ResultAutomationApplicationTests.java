2e88a2219c0f2057a1fdfb4f071284fa7ff3fbf6
hasan.jaman <hasan.jaman@bjitgroup.com>
2019-08-28 12:44:12 +0600

    TDD enabled

--- src/test/java/com/lighthouse/resultautomation/ResultAutomationApplicationTests.java
+++ src/test/java/com/lighthouse/resultautomation/ResultAutomationApplicationTests.java
@@ -1,6 +1,5 @@
 package com.lighthouse.resultautomation;
 
-import com.lighthouse.resultautomation.model.User;
 import com.lighthouse.resultautomation.repository.UserRepository;
 import com.lighthouse.resultautomation.service.AuthService;
 import org.junit.Test;
@@ -8,28 +7,37 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 
-import java.util.Optional;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ResultAutomationApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ResultAutomationApplicationTests {
-
 	@Autowired
 	private AuthService authService;
-
 	@MockBean
 	private UserRepository userRepository;
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:"+port;
+	}
 
 	@Test
-	public void signUpTest() {
-//		String email = "hnj@gmail.com";
-//		Optional<User> found = userRepository.findByEmail(email);
-//
-//		org.junit.Assert.(found.get().getEmail())
-//				.isEqualTo(email);
-//		Assert.notNull(oil, "oil mustn't be null");
+	public void getAllUserTest(){
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/users",
+				HttpMethod.GET, entity, String.class);
+		Assert.notNull(response.getBody());
 	}
 }
